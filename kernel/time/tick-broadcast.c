1d3ec9183f008761bbff990d681034fecf947d20
Benowit <benowit@gmail.com>
2015-08-22 11:33:54 +0200

    Fix build error

--- kernel/time/tick-broadcast.c
+++ kernel/time/tick-broadcast.c
@@ -87,6 +87,8 @@ static bool tick_check_broadcast_device(struct clock_event_device *curdev,
 void tick_install_broadcast_device(struct clock_event_device *dev)
 {
 
+	struct clock_event_device *cur = tick_broadcast_device.evtdev;
+
 	if (!tick_check_broadcast_device(cur, dev))
 		return;
 
